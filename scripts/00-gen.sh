42613caccc484b4c7b852f878d84e3283b7a2b75
Reach <reach@42paris.fr>
2025-07-14 01:08:22 +0200

    feat: add set -euo pipefail to all scripts

--- scripts/00-gen.sh
+++ scripts/00-gen.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Exit at first command error, undefined variable, or pipe error
+# See https://gist.github.com/mohanpedala/1e2ff5661761d3abd0385e8223e16425
+set -euo pipefail
+
 # Force context to be repository root
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
