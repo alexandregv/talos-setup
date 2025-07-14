d4c991d2773a1f7e842c8d140028f6df5d869213
Reach <reach@42paris.fr>
2025-07-14 03:09:54 +0200

    feat: add cilium

--- scripts/00-gen.sh
+++ scripts/00-gen.sh
@@ -14,6 +14,21 @@ source variables.env
 ips=(${CONTROLPLANES_IPS//,/ })
 ip=${ips[0]}
 
+# Generate cilium config and insert it in a talos patch
+helm template cilium cilium/cilium \
+  --version 1.17.5 \
+  --namespace kube-system \
+  --set ipam.mode=kubernetes \
+  --set kubeProxyReplacement=true \
+  --set securityContext.capabilities.ciliumAgent="{CHOWN,KILL,NET_ADMIN,NET_RAW,IPC_LOCK,SYS_ADMIN,SYS_RESOURCE,DAC_OVERRIDE,FOWNER,SETGID,SETUID}" \
+  --set securityContext.capabilities.cleanCiliumState="{NET_ADMIN,SYS_ADMIN,SYS_RESOURCE}" \
+  --set cgroup.autoMount.enabled=false \
+  --set cgroup.hostRoot=/sys/fs/cgroup \
+  --set k8sServiceHost=localhost \
+  --set k8sServicePort=7445 \
+  --set hubble.relay.enabled=true \
+  --set hubble.ui.enabled=true | sed 's/^/        /' >> patches/cilium-patch.yaml
+
 # Generate talos config with variables and patches
 talosctl gen config "${CLUSTER_NAME}" "https://${ip}:6443" \
   --config-patch @patches/cni-patch.yaml \
